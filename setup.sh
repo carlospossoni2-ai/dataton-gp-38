f77c29178fdf8d9f4f0e32b587b54b79f9fc23db
carlospossoni2-ai <carlospossoni2@gmail.com>
2025-09-29 23:45:35 -0300

    update python version

--- setup.sh
+++ setup.sh
@@ -8,8 +8,5 @@ python -m pip install --upgrade pip
 # Instalar dependências do projeto
 pip install -r requirements.txt
 
-# Baixar modelo de português do spaCy
-python -m spacy download pt_core_news_sm
-
 echo "✅ Ambiente pronto! Agora você pode rodar:"
 echo "streamlit run app/app.py"
